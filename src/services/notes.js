5e6c2d51dfb26ca585f4bc43f4f22cf23776885d
anandkarna-anu <anandkarnaanu@gmail.com>
2025-08-24 09:34:52 +0545

    loggin baseurl to see whther if its liading

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,9 +1,12 @@
 import axios from 'axios'
 const baseUrl = import.meta.env.VITE_API_BASE_URL
+console.log('baseUrl in service:', baseUrl) // Add this line here
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then((response) => response.data)
 }
+// ... rest of your code
 
 const create = (newObject) => {
   const request = axios.post(baseUrl, newObject)
